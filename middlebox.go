8352c19965eca42ed5854c03aa6c72b9237e7929
andrewcchu <andy.chunan.chu@gmail.com>
2022-07-15 12:14:24 -0500

    Inital commit middlebox.go

--- middlebox.go
+++ middlebox.go
@@ -3,8 +3,10 @@ package main
 import (
   "syscall"
   "fmt"
+  "log"
+  "net"
   "github.com/google/gopacket"
-  _ "github.com/google/gopacket/layers"
+  "github.com/google/gopacket/layers"
 )
 
 // Convert a uint16 to host byte order (big endian)
@@ -13,7 +15,9 @@ func Htons(v uint16) int {
 }
 
 func main() {
-  sd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, Htons(syscall.ETH_P_ALL), syscall.IPPROTO_ICMP)
+  // Receive on all protocols (ETH_P_ALL)
+  sd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, Htons(syscall.ETH_P_ALL))
+
   if err != nil {
     panic(err)
   }
@@ -22,11 +26,52 @@ func main() {
 
   _, _, err = syscall.Recvfrom(sd, buf, 0)
   if err != nil {
-      panic(err)
+    panic(err)
   }
+  // Form packet from incoming buffer
   packet := gopacket.NewPacket(buf, layers.LayerTypeEthernet, gopacket.Default)
 
-  for _, layer := range packet.Layers() {
-      fmt.Println("Layer: ", layer.LayerType())
+  fmt.Println(packet) // Print showing read contents TODO: Remove on prod.
+
+  // If TCP packet, modify fields to be correct receiving response L4S flags, and forward back to source
+  if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+    ip, _ := packet.Layer(layers.LayerTypeIPv4).(*layers.IPv4)
+    // If DSCP not set, support L4S
+    if ip.TOS == 0 {
+      ip.TOS = 1
+    }
+    // ip.DstIP = net.IPv4(128, 135, 98, 173)
+    tcp, _ := tcpLayer.(*layers.TCP)
+    tcp.NS = true
+    tcp.CWR = true
+    tcp.ECE = true
+
+    fmt.Println(packet) // Print showing modified contents TODO: Remove on prod.
+
+    // Craft modified packet to forward
+    sndBuff := gopacket.NewSerializeBuffer()
+    options := gopacket.SerializeOptions{
+      FixLengths:       true,
+    }
+    if err = gopacket.SerializePacket(sndBuff, options, packet); err != nil {
+      panic(err)
+    }
+
+    sndPkt := sndBuff.Bytes()
+    hex := fmt.Sprintf("%x", sndPkt)
+    fmt.Println(sndPkt)
+    fmt.Println(hex)
+
+    fd, _ := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_RAW)
+    addr := syscall.SockaddrInet4{
+      Port: int(tcp.DstPort),
+      Addr: [4]byte{ip.DstIP[0], ip.DstIP[1], ip.DstIP[2], ip.DstIP[3]},
+      // Addr: [4]byte{128, 135, 98, 173},
+    }
+    err = syscall.Sendto(fd, sndPkt, 0, &addr)
+    if err != nil {
+      log.Fatal("Sendto:", err)
+    }
+    // TODO: Write client to try readint packet recv'd at port
   }
 }
